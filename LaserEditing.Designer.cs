50eb0d167137f2b898ca5d92c49ce991fb825aa0
Ding Yuan <110959751@qq.com>
2025-07-19 17:01:34 +0800

    7-19-3

---
+++ LaserEditing.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace MusicChange
+{
+	partial class LaserEditing
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.修改图片 = new DevComponents.DotNetBar.ButtonX();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.splitContainer4 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer5 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer6 = new System.Windows.Forms.SplitContainer();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+			this.splitContainer3.Panel1.SuspendLayout();
+			this.splitContainer3.Panel2.SuspendLayout();
+			this.splitContainer3.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
+			this.splitContainer4.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).BeginInit();
+			this.splitContainer5.Panel2.SuspendLayout();
+			this.splitContainer5.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer6)).BeginInit();
+			this.splitContainer6.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.splitContainer1.Panel1.Controls.Add(this.修改图片);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+			this.splitContainer1.Size = new System.Drawing.Size(1753, 1033);
+			this.splitContainer1.SplitterDistance = 46;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// 修改图片
+			// 
+			this.修改图片.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+			this.修改图片.BackColor = System.Drawing.Color.Green;
+			this.修改图片.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
+			this.修改图片.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(120)));
+			this.修改图片.Location = new System.Drawing.Point(77, 3);
+			this.修改图片.Name = "修改图片";
+			this.修改图片.Size = new System.Drawing.Size(160, 33);
+			this.修改图片.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2013;
+			this.修改图片.Symbol = "";
+			this.修改图片.SymbolColor = System.Drawing.Color.Maroon;
+			this.修改图片.SymbolSize = 12F;
+			this.修改图片.TabIndex = 0;
+			this.修改图片.Text = "处理图片";
+			this.修改图片.TextColor = System.Drawing.Color.Black;
+			this.修改图片.Click += new System.EventHandler(this.修改图片_Click);
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.splitContainer2.Panel2.Controls.Add(this.splitContainer5);
+			this.splitContainer2.Size = new System.Drawing.Size(1753, 983);
+			this.splitContainer2.SplitterDistance = 585;
+			this.splitContainer2.TabIndex = 0;
+			// 
+			// splitContainer3
+			// 
+			this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer3.Name = "splitContainer3";
+			// 
+			// splitContainer3.Panel1
+			// 
+			this.splitContainer3.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.splitContainer3.Panel1.Controls.Add(this.panel4);
+			this.splitContainer3.Panel1.Controls.Add(this.panel3);
+			this.splitContainer3.Panel1.Controls.Add(this.panel1);
+			// 
+			// splitContainer3.Panel2
+			// 
+			this.splitContainer3.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.splitContainer3.Panel2.Controls.Add(this.splitContainer4);
+			this.splitContainer3.Size = new System.Drawing.Size(1753, 585);
+			this.splitContainer3.SplitterDistance = 672;
+			this.splitContainer3.TabIndex = 0;
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel4.Location = new System.Drawing.Point(137, 71);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(535, 514);
+			this.panel4.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel3.Location = new System.Drawing.Point(0, 71);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(137, 514);
+			this.panel3.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(672, 71);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(672, 71);
+			this.panel2.TabIndex = 1;
+			// 
+			// splitContainer4
+			// 
+			this.splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer4.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer4.Name = "splitContainer4";
+			// 
+			// splitContainer4.Panel2
+			// 
+			this.splitContainer4.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.splitContainer4.Size = new System.Drawing.Size(1077, 585);
+			this.splitContainer4.SplitterDistance = 562;
+			this.splitContainer4.TabIndex = 0;
+			// 
+			// splitContainer5
+			// 
+			this.splitContainer5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.splitContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer5.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer5.Name = "splitContainer5";
+			this.splitContainer5.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer5.Panel1
+			// 
+			this.splitContainer5.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			// 
+			// splitContainer5.Panel2
+			// 
+			this.splitContainer5.Panel2.Controls.Add(this.splitContainer6);
+			this.splitContainer5.Size = new System.Drawing.Size(1753, 394);
+			this.splitContainer5.SplitterDistance = 32;
+			this.splitContainer5.TabIndex = 0;
+			// 
+			// splitContainer6
+			// 
+			this.splitContainer6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer6.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer6.Name = "splitContainer6";
+			// 
+			// splitContainer6.Panel1
+			// 
+			this.splitContainer6.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.splitContainer6.Size = new System.Drawing.Size(1753, 358);
+			this.splitContainer6.SplitterDistance = 584;
+			this.splitContainer6.TabIndex = 0;
+			// 
+			// LaserEditing
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1753, 1033);
+			this.Controls.Add(this.splitContainer1);
+			this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.Location = new System.Drawing.Point(-1300, 280);
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "LaserEditing";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Form3";
+			this.Load += new System.EventHandler(this.LaserEditing_Load);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LaserEditing_MouseDown);
+			this.Resize += new System.EventHandler(this.LaserEditing_Resize);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.splitContainer3.Panel1.ResumeLayout(false);
+			this.splitContainer3.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+			this.splitContainer3.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
+			this.splitContainer4.ResumeLayout(false);
+			this.splitContainer5.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).EndInit();
+			this.splitContainer5.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer6)).EndInit();
+			this.splitContainer6.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.SplitContainer splitContainer3;
+		private System.Windows.Forms.SplitContainer splitContainer4;
+		private System.Windows.Forms.SplitContainer splitContainer5;
+		private System.Windows.Forms.SplitContainer splitContainer6;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private DevComponents.DotNetBar.ButtonX 修改图片;
+	}
+}
---
+++ LaserEditing.cs
@@ -0,0 +1,108 @@
+﻿#region 系统加载部分
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace MusicChange
+{
+	public partial class LaserEditing:Form
+	{
+		private bool splitContainer5mouseDown;
+		int count = 0;
+
+	
+		public LaserEditing()
+		{
+			
+			InitializeComponent();
+			#endregion
+			//// 固定左侧分区宽度
+			//splitContainer5.FixedPanel = FixedPanel.Panel1;
+			//splitContainer5.SplitterDistance = 130; // 左侧宽度 200px
+			////// 可选：限制最小宽度（防止用户拖得过小）
+			////splitContainer5.Panel1MinSize = 135; // 左侧最小宽度 150px[4,5](@ref)
+			////// 可选：禁用拖动
+			//splitContainer5.IsSplitterFixed = true;
+			////splitContainer5.Width = 600;
+
+			////splitContainer5.Panel2MinSize = 400;
+			///
+		
+		}
+		private void LaserEditing_Load(object sender, EventArgs e)
+		{
+			splitContainer5mouseDown = false;
+			splitContainer1.Panel2MinSize = 400;
+		}
+
+
+
+	
+
+		private void LaserEditing_MouseDown(object sender, MouseEventArgs e)
+		{
+			splitContainer5mouseDown = true;
+		}
+
+		private void splitContainer5_MouseDown(object sender, MouseEventArgs e)
+		{
+			splitContainer5mouseDown = true;
+		}
+
+	
+		private void splitContainer3_Panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void LaserEditing_Resize(object sender, EventArgs e)
+		{
+			splitContainer1.Panel2MinSize = 400;
+		}
+
+		private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void 修改图片_Click(object sender, EventArgs e)
+		{
+			ChangePictuer form = new ChangePictuer();
+           form.Show();
+
+		}
+
+		//private void SplitContainer5_SplitterMoving(object sender, SplitterCancelEventArgs e)
+		//{
+		//	// 仅处理水平分隔条（Orientation=Vertical）
+		//	if (splitContainer1.Orientation == Orientation.Vertical) {
+		//		// 计算 Panel2 当前宽度：总宽度 - 分隔条位置
+		//		int panel2Width = splitContainer1.Width - e.SplitX;
+		//		// 若 Panel2 宽度 < 400px，则取消移动
+		//		if (panel2Width < 400) {
+		//			e.Cancel = true;  // 阻止移动
+		//			splitContainer1.SplitterDistance = splitContainer1.Width - 400; // 重置位置
+		//		}
+		//	}
+		//}
+
+
+	}
+}
